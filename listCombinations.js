d6c11128b79aa141c491f5b07ac2881dbcabc1e9
Bo Chen <bochen2014@yahoo.com>
2018-02-09 22:19:56 +1100

    list combination AT

--- listCombinations.js
+++ listCombinations.js
@@ -1,18 +1,18 @@
 function combine(n, k) {
-    if(n<1 || k < 1 || n<k){
+    if (n < 1 || k < 1 || n < k) {
         return [];
     }
-    if(k=1){
-        return Array.from(Array(n), (_, i)=>[i+1]);
+    if (k === 1) {
+        return Array.from(Array(n), (_, i) => [i + 1]);
     }
-    var _nSelected = combine(n-1, k-1);
-    for(var i=0;i<_nSelected.length; i++){
-       _nSelected[i].push(n);
+    var _nSelected = combine(n - 1, k - 1);
+    for (var i = 0; i < _nSelected.length; i++) {
+        _nSelected[i].push(n);
     }
-    var _nNotSelected = combine(n-1, k);
+    var _nNotSelected = combine(n - 1, k);
     return _nSelected.concat(_nNotSelected);
-    
-    
+
+
 };
 
-console.log(combine(4, 2))
+console.log(combine(5, 3))
