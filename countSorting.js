f4234512441a82305af9452daf0742e62d7c542b
Bo Chen <bo.chen@vodafone.com.au>
2018-02-09 15:55:13 +1100

    update

--- countSorting.js
+++ countSorting.js
@@ -2,11 +2,11 @@ const input = [1, 3, 2, 2, 3, 3, 1, 1, 3, 3, 2]
 
 function countSorting(arr, max) {
     const hash = {};
-    for (var i in arr) {
+    for (var i in arr) { // Time complexity: O(n)
         hash[arr[i]] = (hash[arr[i]] || 0) + 1;
     }
 
-    const result = [];
+    const result = []; // space complexity: O(n)
     for (var i = 1; i <= max; i++) {
         for (var j = 0; j < hash[i]; j++) {
             result.push(i);
