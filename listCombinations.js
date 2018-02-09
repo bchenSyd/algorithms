0d6d731a245690c559e0c5698c8fc8fcb59673e3
Bo Chen <bochen2014@yahoo.com>
2018-02-09 22:08:16 +1100

    Array.from(Array(n), (_, i)=>i+1)

---
+++ listCombinations.js
@@ -0,0 +1,18 @@
+function combine(n, k) {
+    if(n<1 || k < 1 || n<k){
+        return [];
+    }
+    if(k=1){
+        return Array.from(Array(n), (_, i)=>[i+1]);
+    }
+    var _nSelected = combine(n-1, k-1);
+    for(var i=0;i<_nSelected.length; i++){
+       _nSelected[i].push(n);
+    }
+    var _nNotSelected = combine(n-1, k);
+    return _nSelected.concat(_nNotSelected);
+    
+    
+};
+
+console.log(combine(4, 2))
