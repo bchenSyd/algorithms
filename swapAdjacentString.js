cd3391408ac6dee0c226e4ef2dcefbdd00608ca2
Bo Chen <bochen2014@yahoo.com>
2018-02-10 14:18:52 +1100

    string sort

--- swapAdjacentString.js
+++ swapAdjacentString.js
@@ -1,33 +1,30 @@
-const input1 = 'RXXLRXRRXL';
-const input2 = 'XRXLXRRXRR';
+const input1 = 'XLLR'; // sort result should be 'LLRX'
+const input2 = 'LXLX'; // sort result should be 'LLXX'
 
 /**
  * return whether arr2 can be the result of swapping chars in arr1
  * @param {string} arr1 
  * @param {string} arr2 
  */
-function canBeSwapped(arr1, arr2) {
-    if (arr1.length === 0 && arr2.length === 0) {
-        return true;
-    }
-    if (arr1.length !== arr2.length) {
-        return false;
-    }
-
-    let i = 0, n = arr1.length;
-    while (i < n) {
-        if (arr1[i] === arr2[i]) {
-            // arr1 and arr2 have same char at position i;
-            i++;
-        } else if (arr1[i] === arr2[i + 1] && arr1[i + 1] === arr2[i]) {
-            // not the same, but can be swapped
-            i += 2;
-        } else {
-            return false;
-        }
-    }
-    return true;
+function canBeSwapped(str1, str2) {
+    return sort(str1) === sort(str2);
 
 }
 
+function sort(str) {
+    if (str.length < 2) {
+        return str;
+    }
+    const result = [];
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] !== 'X') {
+            result.push(str[i]);
+        }
+    }
+    const missing = str.length - result.length;
+    for (var i = 0; i < missing; i++) {
+        result.push('X');
+    }
+    return result.join('');
+}
 console.log(canBeSwapped(input1, input2))
