4e66667555f2427068477108c6daae1f5fe1e247
Bo Chen <bochen2014@yahoo.com>
2018-02-12 21:40:21 +1100

    update

--- AddListNode.js
+++ AddListNode.js
@@ -19,25 +19,21 @@ const l2 = new ListNode(4);
 function addListNodes(l1, l2) {
     let overflow = 0;
     let i = l1, j = l2;
-    let result = null;
-    let current = null;
+    const dummyHead = new ListNode(0);
+    let current = dummyHead;
     while (i || j) {
         var sum = (i && i.val) + (j && j.val) + overflow;
         overflow = parseInt(sum / 10);
         var node = new ListNode(sum % 10);
         i = i && i.next;
         j = j && j.next;
-        if (current) {
-            current.next = node;
-        }else{
-            result = node;
-        }
+        current.next = node;
         current = node;
     }
-    if(overflow){
-        current.next= new ListNode(overflow);
+    if (overflow) {
+        current.next = new ListNode(overflow);
     }
-    return result;
+    return dummyHead.next;
 }
 
 
