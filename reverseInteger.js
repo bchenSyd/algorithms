a48ef4ea20b0771e8eb86ae2299e7f3f91e11a1b
Bo Chen <bo.chen@vodafone.com.au>
2018-02-14 11:03:57 +1100

    better way of rever integer

--- reverseInteger.js
+++ reverseInteger.js
@@ -2,24 +2,42 @@
  * @param {number} x
  * @return {number}
  */
-var reverse = function(x) {
-    var positive = x>0;
-    var abs= Math.abs(x);
-    const result=[];
-    var weight = 1;
-    while( parseInt(abs/weight) >0 ){
-        var tmp = parseInt(abs/weight);
-        result.push(tmp %10);
-        weight *= 10;
+// var reverse = function(x) {
+//     var positive = x>0;
+//     var abs= Math.abs(x);
+//     const result=[];
+//     var weight = 1;
+//     while( parseInt(abs/weight) >0 ){
+//         var tmp = parseInt(abs/weight);
+//         result.push(tmp %10);
+//         weight *= 10;
+//     }
+//     weight = 1;
+//     var val = 0;
+//     for(var i=result.length -1; i >= 0; i--){
+//         val += result[i] * weight;
+//         weight *=10;
+//     }
+//     if( val >=  Math.pow(2,31)-1){
+//         return 0;
+//     }
+//     return positive ? val : -1 * val;
+// };
+
+
+function reverse(x) {
+    var result = 0;
+
+    while (x != 0) {
+        var tail = x % 10;
+        var newResult = result * 10 + tail;
+        if (parseInt((newResult - tail) / 10) != result) {
+            // overflow
+            return 0;
+        }
+        result = newResult;
+        x = x / 10;
     }
-    weight = 1;
-    var val = 0;
-    for(var i=result.length -1; i >= 0; i--){
-        val += result[i] * weight;
-        weight *=10;
-    }
-    if( val >=  Math.pow(2,31)-1){
-        return 0;
-    }
-    return positive ? val : -1 * val;
-};
+
+    return result;
+}
